5e16fd9dd1096c1011a1d4cf96ffccc81db0bfc0
zjhhome <846155366@qq.com>
2019-05-12 22:19:48 +0800

    块写完了

--- cjsj/测试/test.py
+++ cjsj/测试/test.py
@@ -5,7 +5,7 @@ Created on 2019年5月1日
 '''
 import pymysql
 import jqdatasdk as jq
-import time
+
  
 #jqdata认证
 jq.auth('13401179853','king179853')
@@ -21,8 +21,9 @@ connect = pymysql.Connect(
 )
 cursor = connect.cursor()
 
-#获取总市值和净利润
 
+'''
+#获取总市值和净利润
 date_list=[]
 sql = "SELECT date FROM rqb order by id "
 cursor.execute(sql)
@@ -50,15 +51,37 @@ for k in range(0,len(date_list)):
             total=total+cursor.rowcount
     print(date_list[k][0],'数据插入完毕','共更新了',total,'条数据')
 print("所有数据插入完成")
-
-
-'''
-#最上面的数据是最新的
-df2=jq.get_money_flow(['002197.XSHE',], start_date='2019-05-1', end_date='2019-05-10', fields=['net_amount_xl','net_amount_l','net_amount_m','net_amount_s'])
-print(df2)
 '''
 
 '''
+#获取每日大单量,最上面的数据是最新的
+dmb_list=[]
+sql = "SELECT dm FROM dmb order by id "
+cursor.execute(sql)
+for row in cursor.fetchall():
+    dmb_list.append(row)
+for o in range(0,len(dmb_list)):
+    zjl_df=jq.get_money_flow([dmb_list[o][0]]+'.XSHE', start_date='2015-01-01', end_date='2019-05-15', fields=['date','net_amount_xl','net_amount_l','net_amount_m','net_amount_s'])
+    print(zjl_df)
+    zjl_list=zjl_df.values.tolist()
+    total=0;
+    for p in range(0,len(zjl_list)):
+        try:
+            sql = "update %s set  cdd=%.2f ,  dd=%.2f  ,  zd=%.2f  ,  xd=%.2f  where date='%s'"
+            data_zjl=('TB'+dmb_list[o][0],zjl_list[p][1],zjl_list[p][2],zjl_list[p][3],zjl_list[p][4],zjl_list[p][0])
+            cursor.execute(sql % data_zjl)
+        except Exception as e:
+            connect.rollback()  # 事务回滚
+        else:
+            connect.commit()  # 事务提交
+            total=total+cursor.rowcount
+    print(dmb_list[o][0],'数据插入完毕','共更新了',total,'条数据')
+print("所有数据插入完成")
+'''
+
+
+'''
+#获取十大流通股东
 from jqdatasdk import finance
 df3=finance.run_query(jq.query(
 finance.STK_SHAREHOLDER_FLOATING_TOP10.code,
