dc6445e581b47a45ceaaa321dbff1967e645c892
RiverBankChild <846155366@qq.com>
2019-06-19 14:00:35 +0800

    ss

--- cjsj/测试/test2.py
+++ cjsj/测试/test2.py
@@ -5,18 +5,12 @@ Created on 2019年5月1日
 '''
 import pymysql
 import jqdatasdk as jq
-import time
-from _datetime import datetime
+from jqdatasdk import finance
  
 #jqdata认证
 jq.auth('13401179853','king179853')
 
-#定义当前日期
-d=time.strftime('%Y-%m-%d',time.localtime(time.time())) 
-print(d)
-
-
-#建立连接
+#建立连接# 获取游标
 connect = pymysql.Connect(
     host='localhost',
     port=3306,
@@ -25,27 +19,32 @@ connect = pymysql.Connect(
     db='xg',
     charset='utf8'
 )
-
-# 获取游标
 cursor = connect.cursor()
 
-df1=jq.get_price('000001.XSHG',  end_date='2019-05-10', frequency='daily', fields=['open', 'close', 'high', 'low', 'volume'], skip_paused=True, fq='pre')
-df1.reset_index(inplace=True,drop=False)
-list=df1.values.tolist()
-#插入数据 
 
-zjl_df=jq.get_money_flow(['000001.XSHG'], start_date='2015-01-01', end_date='2019-05-22', fields=['date','change_pct'])
-print(zjl_df)
-zjl_list=zjl_df.values.tolist()
 
-#total=0;
-#for p in range(0,len(zjl_list)):
- #   sql = "update %s set zdf=%.2f, cdd=%.2f ,  dd=%.2f  ,  zd=%.2f  ,  xd=%.2f  where date='%s'"
- #   data=('TB'+dmb_list[o][:6],zjl_list[p][1],zjl_list[p][2],zjl_list[p][3],zjl_list[p][4],zjl_list[p][5],datetime.date(datetime.fromtimestamp(zjl_list[p][0].timestamp())))
- #   cursor.execute(sql % data)
-  #  connect.commit()  # 事务提交
-#    total=total+cursor.rowcount
-#print(dmb_list[o],'数据插入完毕,','共更新了',total,'条数据')
+
+
+
+#获取十大流通股东
+dm_list=[]
+dm_sh_list=[]
+sql = "SELECT dm FROM dmb order by id "
+cursor.execute(sql)
+for row in cursor.fetchall():
+    dm_list.append(row) 
+for s in range(len(dm_list)):
+    if dm_list[s][0].startswith('6'):
+        t=dm_list[s][0]+'.XSHG'
+    else:
+        t=dm_list[s][0]+'.XSHE'        
+    dm_sh_list.append(t)
+for x in range(0,len(dm_sh_list)):
+    df=finance.run_query(
+        jq.query(finance.STK_XR_XD.code,finance.STK_XR_XD.a_xr_date).filter(finance.STK_XR_XD.code==x).order_by(finance.STK_XR_XD.report_date.desc()).limit(1)
+        )
+    print(df)
+
 
 
 # 关闭连接
